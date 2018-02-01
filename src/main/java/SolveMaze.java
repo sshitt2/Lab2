64d03bc44683d39754aa1132d2570cc5e9cd7b88
Sadeeq Shittu <sshitt2@illinois.edu>
2018-01-31 18:31:32 -0600

    Implementation of maze-solver algorithm

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -35,6 +35,13 @@ public class SolveMaze {
          */
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
+            while(maze.isFinished() == false) {
+                if(maze.canMove() == false)
+                    maze.turnRight();
+                else{
+                    maze.turnLeft();
+                }
+            }
         }
 
         if (maze.isFinished()) {
